462e3ca58a664015783b002c8da6ce15f3c25b98
JinHwi Kim <jinhwi521@gmail.com>
2026-04-04 03:34:55 +0900

    feat: deploy jar to EC2

---
+++ src/main/java/hwii/cicdspringpractice/HelloController.java
@@ -0,0 +1,13 @@
+package hwii.cicdspringpractice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD TEST v1";
+    }
+}c
